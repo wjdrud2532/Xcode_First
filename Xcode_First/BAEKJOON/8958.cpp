977ec71fae33d04720ca3c8ba89b9bbaba19ffc8
wjdrud2532 <wjdrud2532@naver.com>
2021-04-13 23:57:31 +0900

    21.04.13

---
+++ Xcode_First/BAEKJOON/8958.cpp
@@ -0,0 +1,30 @@
+//
+//  8958.cpp
+//  Xcode_First
+//
+//  Created by 한정경 on 2021/04/13.
+//
+#include <iostream>
+#include <cstring>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::queue;
+using std::ios_base;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    
+   
+
+}
