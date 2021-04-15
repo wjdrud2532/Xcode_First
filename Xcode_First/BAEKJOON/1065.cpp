1338617a7a04198465f995dacbae79a3f4f52d7c
wjdrud2532 <wjdrud2532@naver.com>
2021-04-15 22:33:33 +0900

    21.04.15

--- Xcode_First/BAEKJOON/1065.cpp
+++ Xcode_First/BAEKJOON/1065.cpp
@@ -1,34 +1,18 @@
+//
+//  1065.cpp
+//  Xcode_First
+//
+//  Created by 한정경 on 2021/04/15.
+//
+
 #include <iostream>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
-//-----------------------------문제풀이는 맞으나 더 깔끔하게 할 수 있다
-/*
-bool HanNumber(int Num)
-{
-    int NowNumber;
-    bool Ishan;
-    NowNumber = Num % 10;
-    Num /= 10;
-    //cout << NowNumber << " " << Num << " dd \n" ;
-    for (int i = -4; i <= 4; i ++)
-    {
-        if (NowNumber + i == Num % 10)
-            while (Num >= 10)
-            {
-                cout << i << " NowNumber = " << NowNumber << "  Num % 10 = " << Num % 10 << " " << endl;
-                if (NowNumber + i != Num % 10)
-                    Ishan = false;
-                NowNumber = Num % 10;
-                Num /= 10;
-            }
-    }
-    
-    return true;
-}
-*/
+//-----------------------------문제풀이는 맞으나 더 깔끔하게 할 수 있을 것이다
+
 bool HanNumber(int Num)
 {
     int Number[4];  //a는 최대 1000, 4자리
