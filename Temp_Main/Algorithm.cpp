bc37fe3de4e01e189d4c1ed4e24990791d47bc82
wjdrud2532 <wjdrud2532@naver.com>
2021-05-03 07:03:24 +0900

    21.05.03

--- Temp_Main/Algorithm.cpp
+++ Temp_Main/Algorithm.cpp
@@ -31,3 +31,36 @@
  
  
  */
+
+
+/*
+ 
+ #include <iostream>
+ #include <utility>
+
+ using std::cin;
+ using std::cout;
+ using std::endl;
+
+ int main() {
+     int num;
+     int rank = 1;
+     std::pair<int,int> arr[50];
+     cin >> num;
+     for(int i = 0; i < num; i++)
+         cin >> arr[i].first >> arr[i].second;
+     for(int i = 0; i < num; i++)
+     {
+         for(int j = 0; j < num; j++)
+             if(arr[i].first < arr[j].first && arr[i].second < arr[j].second)
+                 rank++;
+         cout << rank << ' ';
+         rank = 1;
+     }
+ }
+
+ 
+ 
+ 
+ 
+ */
--- Temp_Main/main.cpp
+++ Temp_Main/main.cpp
@@ -1,47 +1,90 @@
+//https://cryptosalamander.tistory.com/13
+
 #include <iostream>
+#include <cstring>
+#include <utility>
+#include <algorithm>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
+
+                        //최선의 경우를 가지고 이것과 얼마나 다른지 비교하기 위해 선언한다.
+string Black[8] = {
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW"
+};
+string White[8] = {
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB",
+    "BWBWBWBW",
+    "WBWBWBWB"
+};
+
+string Board[50];       //N이 최대 50이므로
+
+int BW_cnt(int x, int y)
+{
+    int cnt = 0;
+    for(int i = 0; i < 8; i ++)     //위에서 선언한 최선의 경우가 8 * 8이므로 8 번씩 반복한다.
+    {
+        for(int j = 0; j < 8; j ++)
+        {
+            if(Board[x + i][y + j] != Black[i][j])  //string의 배열이므로 [x + i] 부분은 한 줄 이고 [y + j] 부분은 그 한 줄에서 특정 문자를 가리키는 것이다.
+                cnt ++;     //최선의 경우와 비교하여 값이 다르다면 cnt를 ++ 한다.
+        }
+    }
+    return cnt;
+}
+
+int WB_cnt(int x, int y)
+{
+    int cnt = 0;
+    for(int i = 0; i < 8; i ++)
+    {
+        for(int j = 0; j < 8; j ++)
+        {
+            if(Board[x + i][y + j] != White[i][j])
+                cnt ++;
+        }
+    }
+    return cnt;
+}
 
 int main()
 {
-    int N;
+    int min_val = 100000;
     
-    cin >> N;
+    std::pair<int, int> p1;     //굳이 pair를 써야하나 싶기도 하다  보기 편하라고 하는건가?
     
-    int arr[N][3];
-    int Max[2] = { 0, 0 };
+    cin >> p1.first >> p1.second;
     
-    for(int i = 0; i < N; i ++)
+    for(int i = 0; i < p1.first; i ++)
     {
-        cin >> arr[i][0] >> arr[i][1];
-        
-        if(arr[i][0] >= Max[0] && arr[i][1] > Max[1])       //가장 큰 값을 Max에 저장
-        {
-            Max[0] = arr[i][0];
-            Max[1] = arr[i][1];
-            arr[i][2] = 1;
-        }
+        cin >> Board[i];        //입력 받음
     }
     
-    int tempNum = 2;
-    int count = 0;
-    
-    for(int i = 0; i < N; i ++)
-    {
-        if(arr[i][0] != Max[0] && arr[i][1] != Max[1])
+    for(int i = 0; i + 8 <= p1.first; i ++) // +8을 하는 이유는 아래서 사용하는 BW, WB_cnt가 8 * 8 탐색, 즉 현재로부터 + 8의 경우까지 탐색하기 때문이다
+    {                                                                   // +8을 하지 않는다면 범위를 벗어나 오류가 난다.
+        for(int j = 0; j + 8 <= p1.second; j ++)
         {
-            for(int j = 0; j < N; j ++)
-            {
-                if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1])
-                {
-                    
-                }
-            }
+            int temp;
+            temp = std::min(BW_cnt(i, j), WB_cnt(i, j));        // 둘 중 더 작은 값을 temp에 할당한다.
+            if(temp < min_val)      //가장 작은 값이라면 다시 할당한다.
+                min_val = temp;
         }
-            
     }
-    
+    cout << min_val;
+    return 0;
 }
-
