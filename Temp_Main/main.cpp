a78335415e887ed5b87d457a5b2cbc9ade5f6506
wjdrud2532 <wjdrud2532@naver.com>
2021-06-18 03:17:47 +0900

    21.06.18

--- Temp_Main/main.cpp
+++ Temp_Main/main.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <cstring>
 
 using std::cin;
 using std::cout;
 using std::ios_base;
+using std::string;
+using std::vector;
+using std::pair;
+
+string temp1, temp2;
+
+int N, M;
+int arr[31];
+int Visit[101];
+
+void dfs(int count, int num)
+{
+    if(count == M)
+    {
+        for(int i = 0; i < M; i ++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    
+    for(int i = num; i <= N; i ++)
+    {
+        if(!Visit[i])
+        {
+            Visit[i] = true;
+            arr[count] = i;         //4가 될때까지 N에 i를 저장
+            dfs(count + 1, ++num);
+            Visit[i] = false;
+        }
+    }
+}
+
+bool isEqual(const pair<string, int>& element)
+{
+    return element.first ==  temp2;
+}
+
 
 int main()
 {
@@ -12,47 +52,50 @@ int main()
     cout.tie(NULL);
     
     int T;
-    int N, M;
-    int dp[31][31];
-    
-    dp[0][0] = dp[1][0] = dp[1][1] = 1;
-    for(int i = 1; i <= 30; i ++)
-    {
-        for(int j = 0; j <= i; j ++)
-        {
-            if(j == 0 || j == i)
-                dp[i][j] = 1;
-            
-            else
-                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]);
-        }
-    }
+    int ClothesNum;
+    vector< pair<string, int> > P;
     
     cin >> T;
     
+    
     for(int i = 0; i < T; i ++)
     {
-        cin >> N >> M;
-        cout << dp[M][N] << "\n";
+        cin >> ClothesNum;
+        for(int j = 0; j < ClothesNum; j ++)
+        {
+            cin >> temp1 >> temp2;
+            auto it = std::find_if( P.begin(), P.end(), isEqual );
+            
+            if(it != P.end())       //있을 경우
+                it->second ++;
+            
+            else                    //없을 경우
+                P.push_back(std::make_pair(temp2, 1));
+        }
+        
+        int arr[P.size()];
+        int Visit[P.size()];
+        
+        
+        P.clear();
+        
     }
+    /*
+     1
+     2
+     3
+     4
+     12
+     13
+     14
+     23
+     24
+     34
+     123
+     124
+     134
+     234
+     1234
+     */
 }
 
-/*
- #include <stdio.h>
-
- int main() {
-     int n, k, t;
-     scanf("%d", &t);
-     for (int i = 0; i < t; i++) {
-         scanf("%d%d", &k, &n);
-         int res = 1, m = 1;
-         while (k >= 1) {
-             res = res * n / m;
-             n--; m++;
-             k--;
-         }
-         printf("%d\n", res);
-     }
-     return 0;
- }
- */
