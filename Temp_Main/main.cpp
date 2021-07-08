688ae4cdeac14e4d181b9090cf429dd1cca3029d
wjdrud2532 <wjdrud2532@naver.com>
2021-07-09 05:19:45 +0900

    21.07.09

--- Temp_Main/main.cpp
+++ Temp_Main/main.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using std::cin;
 using std::cout;
 using std::ios_base;
-
-long long pow(int x, int y, int c)
-{
-    if(y == 1)
-        return x;
-    else
-    {
-        long long temp = pow(x, y / 2, c);
-        
-        if(y % 2)
-            return ((temp*temp) % c * x) % c;
-        else
-            return (temp*temp) % c;
-    }
-}
+using std::vector;
 
 int main()
 {
@@ -26,11 +13,10 @@ int main()
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
     
-    int A, B, C;
-    
-    cin >> A >> B >> C;
-    
-    
-    cout << pow(A % C, B, C) << "\n";
+    int N = 1, K = 1;
+    vector<vector<int>> V;
+    vector<int> V2;
+    int RestNum = 1000000007;
+    //ㄹ
     
 }
