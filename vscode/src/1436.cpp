c9e44a634f91874adceb5b34f620601e4b33cf04
wjdrud2532 <wjdrud2532@naver.com>
2022-07-19 04:40:07 +0900

    2022.07.19.2

--- vscode/src/1436.cpp
+++ vscode/src/1436.cpp
@@ -1,17 +1,12 @@
-
 #include <iostream>
 
 using std::cin;
 using std::cout;
 using std::ios_base;
 
-int temp;
-
 bool IsTripleSix(int num)
 {
     bool IsTripleSix = false;
-
-    temp = 0;
 
     while (num > 0)
     {
@@ -22,7 +17,6 @@ bool IsTripleSix(int num)
         }
 
         num /= 10;
-        temp++;
     }
 
     return IsTripleSix;
@@ -31,36 +25,17 @@ bool IsTripleSix(int num)
 int FindNum(int N)
 {
     int cnt = 0;
-    for (int i = 666;;)
+    for (int i = 666;; i++)
     {
-        if (IsTripleSix(i) == true)
-            cnt++;
+        if(IsTripleSix(i) == true)
+            cnt ++;
 
-        if (cnt == N)
+        if(cnt == N)
             return i;
-
-        if (temp == 0)
-            i += 1000;
-        else
-            {
-                int temp2 = 1;
-                for(int j = temp - 1; j > 0; j --)
-                {
-                    temp2 *= 10;
-                }
-                i += temp2;
-            }
     }
 
     return 0;
 }
-/*
-
-temp가 0일 경우 + 1000?
-        1일 경우 + 1
-        2일 경우 + 10
-        3일 경우 + 100
-*/
 
 int main()
 {
