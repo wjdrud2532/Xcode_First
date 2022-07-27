335828d3c8ab65917bc2cdbd0707b86c84bb109f
wjdrud2532 <wjdrud2532@naver.com>
2022-07-27 23:33:34 +0900

    2022.07.27.2

--- vscode_main.cpp
+++ vscode_main.cpp
@@ -1,90 +1,96 @@
 #include <iostream>
-#include <algorithm>
-#include <cstring>
+#include <vector>
+#include <climits>
 
 using std::cin;
 using std::cout;
 using std::ios_base;
+using std::pair;
+using std::vector;
 
-using std::sort;
-using std::string;
-using std::strncpy;
-
-int N;
-string arrstr[20000];
-
-int CompareAlpa(const string &a, const string &b)
+void Solve()
 {
-    if (a.size() == b.size())
-    {
-        for (int i = 0; i < a.size(); i++)
-        {
-            if (a[i] != b[i])
-                return a[i] < b[i]; // 1이면 a가 먼저 출력
-        }
-    }
-}
+    int N;
+    vector<pair<int, bool>> arr;
 
-bool CompareLength(const string &a, const string &b)
-{
-    if (a.size() < b.size()) // a가 더 짧을 경우 먼저 출력
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+    cin >> N;
 
-void sortWord(int N)
-{
+    pair<int, bool> temppair;
     for (int i = 0; i < N; i++)
     {
-        cin >> arrstr[i];
+        cin >> temppair.first;
+        temppair.second = false;
+        arr.push_back(temppair);
     }
 
-    sort(arrstr, arrstr + N, CompareAlpa);
-    sort(arrstr, arrstr + N, CompareLength);
+    //전부 입력 받음
 
-    bool temp = false;
+    int min = INT_MAX, tempnum;
+    vector<int> temparr;
+    int count = 0;
 
-    for (int i = 0; i < N - 1; i++)
+    for (int i = 0; i < N; i++)
     {
-        if (arrstr[i].size() == arrstr[i + 1].size())
+        min = INT_MAX;
+
+        for (int j = 0; j < N; j++)
         {
-            for (int j = 0; j < arrstr[i].size(); j++)
+            if (arr[j].second == false)
+
             {
-                if (arrstr[i][j] != arrstr[i + 1][j])
+                if (arr[j].first < min)
                 {
-                    temp = true;
-                    break;k
+                    temparr.clear();
+                    min = arr[j].first;
+                    temparr.push_back(j);
+                    //현재 값들 중 가장 작은 값을 찾는다, 더 작은 값이 나올 경우 다시 초기화
+                    //그 값들의 위치를 temparr에 저장한다
+                }
+                else if (arr[j].first == min) //같은 값일 경우 추가만 한다
+                {
+                    temparr.push_back(j);
                 }
             }
-            if (temp == true)
+        }
+
+        //가장 작은 값을 count로 바꾼다
+        for (int j = 0; j < temparr.size(); j++)
+        {
+            if (arr[temparr[j]].second == false)
+
             {
-                i++;
-                temp == false;
+                arr[temparr[j]].first = count;
+                arr[temparr[j]].second = true;
             }
         }
-        cout << arrstr[i] << "\n";
+        count++;
     }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << arr[i].first << " ";
+        // cout << arr[i].first << " " << arr[i].second << "\n";
+    }
+
+    /*
+
+    가장 작은 값이 0
+
+    들어올 때 마다 상대적인 값을 찾는 것은 비효율적이다
+
+    전부 다 입력 받은 뒤 배열을 이중 for문으로 찾아가면서 상대적인 값을 찾는다?
+    */
 
     return;
 }
 
 int main()
 {
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
 
-    cin >> N;
-
-    cout << "          \n";
-    sortWord(N);
-
-    sort(arrstr, arrstr + N, CompareLength);
+    Solve();
 
     return 0;
 }
