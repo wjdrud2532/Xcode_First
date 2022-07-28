0d17c817ffc567ab62471a6a8953e5a7fa48f006
wjdrud2532 <wjdrud2532@naver.com>
2022-07-28 22:06:44 +0900

    2022.07.28.2

--- Desk_Temp/Desk_Temp/main.cpp
+++ Desk_Temp/Desk_Temp/main.cpp
@@ -1,65 +1,86 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstring>
 
 using std::cin;
 using std::cout;
 using std::ios_base;
 
-using std::vector;
-using std::sort;
-using std::find;
+using namespace std;
 
-int N, M, temp;
-vector <int> arrN, arrM;
 
-int BinarySearch(int num)
-{
-    int left = 0, right = N - 1, mid;
 
-    while (left <= right)
-    {
-        mid = (left + right) / 2;
-
-        if (arrN[mid] == num)
-        {
-            return 1;
-        }
-        else if (arrN[mid] > num)
-        {
-            right = mid - 1;
-        }
-        else
-        {
-            left = mid + 1;
-        }
-    }
-
-    return 0;
-}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-     cin >> N;
-    
-    for (int i = 0; i < N; i++)
-    {
-        cin >> temp;
-        arrN.push_back(temp);
-    }
+	int N, M, count = 0, tempcnt;
+	string temp;
+	vector <string> arrN, arrM;
 
-    sort(arrN.begin(), arrN.end());
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> temp;
+		arrN.push_back(temp);
+	}
 
-    cin >> M;
-    for (int i = 0; i < M; i++)
-    {
-        cin >> temp;
-        cout << BinarySearch(temp) << " ";
-    }
+	for (int i = 0; i < M; i++)
+	{
+		cin >> temp;
+		arrM.push_back(temp);
+	}
 
-    return 0;
+	string temp1, temp2;
+	bool isInclude;
+
+	for (int i = 0; i < M; i++)
+	{
+		//비교해야할 단어들
+		isInclude = false;
+		temp1 = arrM[i];
+
+		for (int j = 0; j < N; j++)
+		{
+			temp2 = arrN[j];
+
+			if (temp1.length() == temp2.length())
+			{
+				tempcnt = 0;
+
+				for (int k = 0; k < temp2.length(); k++)
+				{
+					if (temp1[tempcnt] == temp2[k])
+					{
+						tempcnt++;
+						if (tempcnt == temp1.length())
+							break;
+					}
+					else
+					{
+						tempcnt = 0;
+					}
+				}
+				if (tempcnt == temp1.length())
+					isInclude = true;
+
+				if (isInclude == true)
+				{
+					count++;
+					break;
+				}
+			}
+
+
+		}
+		//cout << temp1 << "    " << count << "\n";
+	}
+
+	cout << count << "\n";
+
+	return 0;
 }
