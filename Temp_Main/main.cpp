d78e5d1bb8f67b20928841a59b8dada056cbf19d
wjdrud2532 <wjdrud2532@naver.com>
2022-08-10 00:55:56 +0900

    2022.08.09.2

--- Temp_Main/main.cpp
+++ Temp_Main/main.cpp
@@ -1,36 +1,53 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int dp[31][31];
+int T, N, num;
+string temp;
+vector<pair<string, int>> v;
+
+bool isEqual(const pair<string, int>& element)
+{
+    return element.first == temp;
+}
 
 int main()
 {
     cin.tie(nullptr);
     cout.tie(nullptr);
     ios_base::sync_with_stdio(false);
-    
-    dp[0][0] = dp[0][1] = dp[1][0] = 1;
-    
-    for(int i = 1; i <= 30; i ++)
-    {
-        for(int j = 0; j <= i; j ++)
-        {
-            if(j == 0 || j == i)
-                dp[i][j] = 1;
-            else
-                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
-        }
-    }
-    
-    int T, N, M;
-    
+        
     cin >> T;
-    
+        
     while(T--)
     {
-        cin >> N >> M;
-        cout << dp[M][N] << "\n";
+        cin >> N;
+        while(N--)
+        {
+            cin >> temp >> temp;
+            auto it = find_if(v.begin(), v.end(), isEqual);     //
+            
+            if( it == v.end())
+            {
+                v.push_back(make_pair(temp, 1));
+            }
+            else
+            {
+                it->second ++;
+            }
+        }
+        
+        num = 1;
+        for(int i = 0; i < v.size(); i ++)
+        {
+            num *= v[i].second + 1;     //공식 사용
+        }
+        
+        cout << num - 1 << "\n";
+        
+        v.clear();
+        
     }
     
     
