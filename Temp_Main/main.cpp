02450bfc34c0ad6c385e896052ac753c5431fef3
wjdrud2532 <wjdrud2532@naver.com>
2022-08-20 22:40:44 +0900

    2022.08.20

--- Temp_Main/main.cpp
+++ Temp_Main/main.cpp
@@ -1,55 +1,71 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<math.h>
 
 using namespace std;
 
-int T, N, num;
-string temp;
-vector<pair<string, int>> v;
+int arr[21][21];
+bool isvisit[21];
+int N, minnum = 201, temp, start, link;
 
-bool isEqual(const pair<string, int>& element)
+
+void dfs(int cnt, int m) // x는 카운트 수, pos는 다음 값
 {
-    return element.first == temp;
+    if (cnt == N / 2) // 서로 같은 개수로 비교 가능한 경우
+    {
+        start = link = 0;
+        
+        for (int i = 1; i <= N; i++)
+        {
+            for (int j = 1; j <= N; j++)
+            {
+                if(i != j)      //0 값은 생략
+                {
+                    // 둘 중 한가지 경우만 발생하므로 else if
+                    
+                    //둘 다 방문했다면 기존값에 더한다
+                    //sij 부터 sji 까지 반복하므로 arr[i][j] + arr[j][i]는 하지 않는다
+                    //절반으로 나ㅝ서 arr[i][j] + arr[j][i]를 할 경우 1,2 가 안된다
+                    if (isvisit[i] == true && isvisit[j] == true)
+                        start += arr[i][j];
+
+                    else if (isvisit[i] == false && isvisit[j] == false)
+                        link += arr[i][j];
+                }
+                
+            }
+        }
+        
+        temp = abs(start - link);
+        
+        if (minnum > temp)
+            minnum = temp;
+        
+        return;
+    }
+    
+    for (int i = m; i < N; i++) // dfs   cnt가 n/2가 될 때 까지 반복
+    {                           // n/2 가 되어야만 start와 link를 맞아 떨어지게 비교 가능
+        isvisit[i] = true;
+        dfs(cnt + 1, i + 1);
+        isvisit[i] = false;
+    }
+    
 }
 
 int main()
 {
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-        
-    cin >> T;
-        
-    while(T--)
+    cin >> N;
+    
+    for (int i = 1; i <= N; i++)
     {
-        cin >> N;
-        while(N--)
+        for (int j = 1; j <= N; j++)
         {
-            cin >> temp >> temp;
-            auto it = find_if(v.begin(), v.end(), isEqual);     //
-            
-            if( it == v.end())
-            {
-                v.push_back(make_pair(temp, 1));
-            }
-            else
-            {
-                it->second ++;
-            }
+            cin >> arr[i][j];
         }
-        
-        num = 1;
-        for(int i = 0; i < v.size(); i ++)
-        {
-            num *= v[i].second + 1;     //공식 사용
-        }
-        
-        cout << num - 1 << "\n";
-        
-        v.clear();
-        
     }
     
+    dfs(0, 1); // 카운트 0회부터 숫자는 1부터 시작
     
-    return 0;
+    cout << minnum << "\n";
 }
+
