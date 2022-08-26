14f627b9ea9141e7449a4c176c5582aa9ca70ee2
wjdrud2532 <wjdrud2532@naver.com>
2022-08-26 11:50:59 +0900

    1

--- Desk_Temp/Desk_Temp/main.cpp
+++ Desk_Temp/Desk_Temp/main.cpp
@@ -1,37 +1,48 @@
 #include<iostream>
-#include <cmath>
+#include<cmath>
 
 using namespace std;
 
-
-int arr[1000000] = { 0, };
-int maxvalue = -2001;
+int n;
+int arr[501][501] = { 0, };
+int dp[501][501];
+int result = 0;
+int temp;
 
 int main()
 {
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios_base::sync_with_stdio(false);
 
-    int n;
-    cin >> n;
+	cin >> n;
 
-    cin >> arr[0];
-    int maxvalue = arr[0];
-    int temp;
-    for (int i = 1; i < n; i++)
-    {
-        cin >> temp;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cin >> temp;
 
-        arr[i] = max(arr[i - 1] + temp, temp);
+			if (j == 1)
+				dp[i][j] = dp[i - 1][j] + temp;
 
-        if (arr[i] > maxvalue)
-            maxvalue = arr[i];
+			else if (j == i)
+				dp[i][j] = dp[i - 1][j - 1] + temp;
 
-    }
+			else
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + temp;
+		}
+	}
 
-    cout << maxvalue << "\n";
+	for (int i = 1; i <= n; i++)
+	{
+		if (dp[n][i] > result)
+			result = dp[n][i];
+	}
 
-    return 0;
+	cout << result << "\n";
+
+
+	return 0;
 }
 
---
+++ vscode/src/1932.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int n;
+int dp[501][501];
+int result = 0;
+int temp;
+
+int main()
+{
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cin >> temp;
+
+			if (j == 1)
+				dp[i][j] = dp[i - 1][j] + temp;
+
+			else if (j == i)
+				dp[i][j] = dp[i - 1][j - 1] + temp;
+
+			else
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + temp;
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (dp[n][i] > result)
+			result = dp[n][i];
+	}
+
+	cout << result << "\n";
+
+
+	return 0;
+}
+
