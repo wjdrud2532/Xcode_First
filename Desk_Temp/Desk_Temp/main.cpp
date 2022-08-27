f92579c1384cababc9ef624c39b49dc7a49e7b38
wjdrud2532 <wjdrud2532@naver.com>
2022-08-27 09:34:52 +0900

    1

--- Desk_Temp/Desk_Temp/main.cpp
+++ Desk_Temp/Desk_Temp/main.cpp
@@ -1,48 +1,28 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int n;
-int arr[501][501] = { 0, };
-int dp[501][501];
-int result = 0;
-int temp;
 
-int main()
-{
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-	ios_base::sync_with_stdio(false);
+int main() {
+
+	int n, arr[300] = { 0, };
+	int dp[300] = { 0, };
 
 	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
 
-	for (int i = 1; i <= n; i++)
+	dp[0] = arr[0];
+	dp[1] = arr[0] + arr[1];
+	dp[2] = max(arr[0], arr[1]) + arr[2];
+
+	for (int i = 3; i < n; i++)
 	{
-		for (int j = 1; j <= i; j++)
-		{
-			cin >> temp;
-
-			if (j == 1)
-				dp[i][j] = dp[i - 1][j] + temp;
-
-			else if (j == i)
-				dp[i][j] = dp[i - 1][j - 1] + temp;
-
-			else
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + temp;
-		}
+		dp[i] = max(dp[i - 2], arr[i - 1] + dp[i - 3]) + arr[i];
 	}
 
-	for (int i = 1; i <= n; i++)
-	{
-		if (dp[n][i] > result)
-			result = dp[n][i];
-	}
-
-	cout << result << "\n";
-
+	cout << dp[n - 1] << "\n";
 
 	return 0;
 }
-
