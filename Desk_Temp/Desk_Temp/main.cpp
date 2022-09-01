0a5bf8d2b2479a49df2831bbc4c737b6577f26c6
wjdrud2532 <wjdrud2532@naver.com>
2022-09-01 09:22:51 +0900

    1

--- Desk_Temp/Desk_Temp/main.cpp
+++ Desk_Temp/Desk_Temp/main.cpp
@@ -6,23 +6,7 @@ using namespace std;
 
 int main() {
 
-	int n, arr[300] = { 0, };
-	int dp[300] = { 0, };
-
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> arr[i];
-
-	dp[0] = arr[0];
-	dp[1] = arr[0] + arr[1];
-	dp[2] = max(arr[0], arr[1]) + arr[2];
-
-	for (int i = 3; i < n; i++)
-	{
-		dp[i] = max(dp[i - 2], arr[i - 1] + dp[i - 3]) + arr[i];
-	}
-
-	cout << dp[n - 1] << "\n";
+	
 
 	return 0;
 }
---
+++ vscode/src/1463.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+	
+	vector<int> dp(n + 1]);
+
+	for (int i = 2; i < n; i++)
+	{
+		dp[i] = dp[i - 1] + 1;
+		//dp[2] = 1로 시자거
+
+		if (i % 2 == 0)
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+		//2로 나뉜다면 현재값, -1해서 2로 나눈 값 중 더 작은 값을 선택
+
+		if (i % 3 == 0)
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+		//마찬가지
+	}
+
+	cout << dp[n] << "\n";
+
+	return 0;
+}
