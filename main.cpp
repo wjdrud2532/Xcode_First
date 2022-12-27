602188e584c26bb96cca3eb0c58dce3f5c416284
wjdrud2532 <wjdrud2532@naver.com>
2022-12-28 02:36:08 +0900

    22.12.28

--- main.cpp
+++ main.cpp
@@ -1,95 +1,43 @@
 #include <iostream>
-#include <cstring>
-#include <bitset>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
-int N, K;
-int bit = 0;
-int arr = 0;
-bool alpa[26] = {0, };
-int alpasize = 0;
+string s1, s2;
 
-void dfs(int count)
+string dp[1001][1001];
+
+void findLCS(string s1, string s2)
 {
-    if(count == K - 5)
-    {
-        for(int i = 0; i < M; i ++)
-        {
 
+    for (int i = 1; i <= s1.length() + 1; i++)
+    {
+        for (int j = 1; j <= s2.length() + 1; j++)
+        {
+            if (s1[i - 1] == s2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + s1[i - 1];
+            else
+                dp[i][j] = (dp[i - 1][j].length() > dp[i][j - 1].length() ? dp[i - 1][j] : dp[i][j - 1]);
         }
     }
 
-    return ;
+    return;
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin >> s1 >> s2;
 
-    //cout << "start\n";
+    findLCS(s1, s2);
 
-    cin >> N >> K;
-
-    if (K < 5)
+    if (dp[s1.length()][s2.length()].length() != 0)
     {
+        cout << dp[s1.length()][s2.length()].length() << "\n";
+        cout << dp[s1.length()][s2.length()] << "\n";
+    }
+    else
         cout << "0\n";
-        return 0;
-    }
-
-    if(K == 26)
-    {
-        cout << N << "\n";
-        return 0;
-    }
-
-//    bit |= 1 << ('a' - 'a');
-//    bit |= 1 << ('n' - 'a');
-//    bit |= 1 << ('t' - 'a');
-//    bit |= 1 << ('c' - 'a');
-//    bit |= 1 << ('t' - 'a');
-
-    alpa['a' - 'a'] = true;
-    alpa['n' - 'a'] = true;
-    alpa['t' - 'a'] = true;
-    alpa['c' - 'a'] = true;
-    alpa['i' - 'a'] = true;
-
-    for (int i = 0; i < N; i++)
-    {
-        string s;
-        cin >> s;
-
-        for (int j = 0; j < s.length(); j++)
-        {
-            if (j < 4)
-                continue;
-
-            if (j > s.length() - 5)
-                break;
-
-            //  사용된 알파벳 기록
-            //arr |= 1 << (s[j] - 'a');
-            alpa[s[j] - 'a'] = true;
-        }
-//        cout << bitset<26>(arr) << " ";
-//        cout << "\n";
-    }
-
-    //새로 들어온 값들의 조합을 구한다
-    for(int i = 0; i < K - 5; i ++)
-    {
-
-    }
-
-
-    //  26개 중 K개의 1을 갖는 비트를 전부 돌린다 -> 너무 느림
-
-    //  두 bit를 조합해서 1이 k개가 되는 경우 이것으로 and 연산을 통해 통과하는 값을 구한다?
-
-
 
     return 0;
 }
